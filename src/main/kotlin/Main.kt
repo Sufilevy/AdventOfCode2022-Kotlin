0995213fd81fd6d9578d6020700bdd11ba5776e6
Sufilevy <sufilevy@gmail.com>
2023-04-03 14:29:54 +0300

    Finished day 12

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,6 @@ fun main() {
 	val input = File("input.txt").readLines()
 //	val input = File("input.txt").readText()
 
-	println(Day3.puzzle1(input))
-	println(Day3.puzzle2(input))
+	println(Day12.puzzle1(input))
+	println(Day12.puzzle2(input))
 }
